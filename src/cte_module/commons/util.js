59857c0577df6819059b14acb4dd6ae928ed847b
Fernando Konflanz <konflanz.info@gmail.com>
2021-09-09 16:13:58 -0300

    Update 09092021.1
    - Adicionando estrutura de diretórios;
    
    - Adicionando módulo do Axios;
    
    - Adicionando método padrão de comunicação com a API da NS Tecnologia
    
    - Adicionando utilitários da biblioteca;

---
+++ src/cte_module/commons/util.js
@@ -0,0 +1,72 @@
+var fs = require('fs');
+const path = require('path')
+
+function dhEmiGet() {
+
+    let dhEmi = new Date()
+    dhEmi = dhEmi.toISOString().slice(0, 11) + dhEmi.toLocaleTimeString() + "-03:00"
+
+    return dhEmi
+}
+
+function gravarLinhaLog(registro) {
+
+    let logTime = new Date()
+    logTime = logTime.toLocaleTimeString() + ":" + logTime.getMilliseconds()
+
+    var caminhoLog = "CTe/logs"
+
+    var fileName = new Date().toISOString().slice(0, 10).replace("-", "").replace("-", "")
+
+    try {
+
+        if (!fs.existsSync(caminhoLog)) {
+            try {
+                salvarArquivo(caminhoLog, fileName, ".log", logTime + " " + registro + "\r\n")
+            }
+            catch (error) {
+                console.log(error)
+            }
+        }
+
+        else {
+
+            fs.appendFile(path.join(caminhoLog, fileName + ".log"), logTime + " " + registro + "\r\n", function (err) {
+
+                if (err) {
+                    // append failed
+                }
+
+                else {
+                    // done
+                }
+            })
+        }
+    }
+
+    catch (err) {
+        console.log(err);
+    }
+}
+
+async function salvarArquivo(caminho, nomeArquivo, extensao, conteudo) {
+
+    var caminhoSalvar = path.join(caminho, nomeArquivo + extensao)
+
+    try {
+        if (!fs.existsSync(caminho)) {
+            fs.mkdirSync(caminho);
+        }
+    }
+
+    catch (err) {
+        console.log(err);
+    }
+
+    fs.writeFile(caminhoSalvar, conteudo, function (err) {
+        if (err) throw err;
+    });
+
+}
+
+module.exports = { salvarArquivo, dhEmiGet, gravarLinhaLog }
