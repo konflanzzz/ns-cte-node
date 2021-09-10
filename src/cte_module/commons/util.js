614653b14f984d9b5a9f46e6f66fb88092874581
Fernando Konflanz <konflanz.info@gmail.com>
2021-09-10 18:07:26 -0300

    Update 10092021.7
    - Implementando método para geração de hash SHA1 para comprovante de entrega

--- src/cte_module/commons/util.js
+++ src/cte_module/commons/util.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 const path = require('path')
+var sha1 = require('sha1');
 
 function dhEmiGet() {
 
@@ -68,5 +69,19 @@ async function salvarArquivo(caminho, nomeArquivo, extensao, conteudo) {
     });
 
 }
+
+function gerarHashCompEntrega(chave, imagem){
+        
+    var base64Imagem = fs.readFileSync(imagem, { encoding: 'base64' });
+    var hashSHA1 = sha1(chave)
+    console.log(hashSHA1)
+    var stringBase64 = new Buffer.from(hashSHA1).toString('base64')
+
+    return stringBase64
+}
+
+var teste = gerarHashCompEntrega("43210907364617000135570000000023181000003300", "C:/Users/fernando.konflanz/GitHub/cte-node-js/src/cte_module/commons/entrega.jpg")
+console.log(teste)
+
 
 module.exports = { salvarArquivo, dhEmiGet, gravarLinhaLog }
